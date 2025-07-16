e8614d33d75d319d68b4a3bfa31d3bc804c3c7d3
IanGolvach <golvachi@msoe.edu>
2025-07-16 13:29:17 -0500

    Make reads mesure like the other one

--- module1/btreebenchmarkreadsMEM.c
+++ module1/btreebenchmarkreadsMEM.c
@@ -13,42 +13,10 @@ int main(void){
     printf("Current UStime = %lu\n", start.tv_sec * 1000000  + start.tv_usec);
     uint8_t addKey[64], addRet[64], addPrev[64];
     // Write 100 trials
-    // printf("100 Read Trial 1\n");
-    // FILE* fp = fopen("benchmark100-1.btree","r+");
-    // uint64_t sumTime = 0;
-    // for(long long i = 0; i < 100; i++){
-    //     for(int j = 0; j < 64; j++){
-    //         addKey[j] = rand();
-    //         memset(addRet, 0, 64);
-    //     }
-    //     gettimeofday(&start, NULL);
-    //     printf("%lli/100: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
-    //     gettimeofday(&stop, NULL);
-    //     sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/100;
-    // }
-    // printf("\nMicrosecond Average Final = %lu\n100 Read Trial 2\n",sumTime);
-    // fclose(fp);
-    FILE* fp = fopen("\nbenchmark100-2.btree","r+");
-    uint64_t size_mb;
-    uint8_t* mb;
-    btree_readDB(fp, &mb, &size_mb);
+    printf("100 Read Trial 1\n");
+    FILE* fp = fopen("benchmark100-1.btree","r+");
     uint64_t sumTime = 0;
-    for(long long i = 0; i < 100; i++){
-        for(int j = 0; j < 64; j++){
-            addKey[j] = rand();
-            memset(addRet, 0, 64);
-        }
-        gettimeofday(&start, NULL);
-        printf("%lli/100: Result of attempting to find random key: %u\r", i, btree_findkey(&mb, addKey, addRet));
-        gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/100;
-    }
-    free(mb);
-    fclose(fp);
-    printf("\nMicrosecond Average Final = %lu\n100 Read Trial 3\n",sumTime);
-    fp = fopen("benchmark100-3.btree","r+");
-    btree_readDB(fp, &mb, &size_mb);
-    sumTime = 0;
+    uint64_t subTotal = 0;
     for(long long i = 0; i < 100; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -57,8 +25,46 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/100: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/100;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 100;
+    printf("\nMicrosecond Average Final = %lu\n100 Read Trial 2\n",sumTime);
+    fclose(fp);
+    FILE* fp = fopen("\nbenchmark100-2.btree","r+");
+    uint64_t size_mb;
+    uint8_t* mb;
+    btree_readDB(fp, &mb, &size_mb);
+    sumTime = 0;
+    subTotal = 0;
+    for(long long i = 0; i < 100; i++){
+        for(int j = 0; j < 64; j++){
+            addKey[j] = rand();
+            memset(addRet, 0, 64);
+        }
+        gettimeofday(&start, NULL);
+        printf("%lli/100: Result of attempting to find random key: %u\r", i, btree_findkey(&mb, addKey, addRet));
+        gettimeofday(&stop, NULL);
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    }
+    sumTime = subTotal / 100;
+    free(mb);
+    fclose(fp);
+    printf("\nMicrosecond Average Final = %lu\n100 Read Trial 3\n",sumTime);
+    fp = fopen("benchmark100-3.btree","r+");
+    btree_readDB(fp, &mb, &size_mb);
+    sumTime = 0;
+    subTotal = 0;
+    for(long long i = 0; i < 100; i++){
+        for(int j = 0; j < 64; j++){
+            addKey[j] = rand();
+            memset(addRet, 0, 64);
+        }
+        gettimeofday(&start, NULL);
+        printf("%lli/100: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
+        gettimeofday(&stop, NULL);
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    }
+    sumTime = subTotal / 100;
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n1000 Read Trial 1\n",sumTime);
@@ -66,6 +72,7 @@ int main(void){
     fp = fopen("benchmark1000-1.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 1000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -74,14 +81,16 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/1000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 1000;
     free(mb);
     printf("\nMicrosecond Average Final = %lu\n1000 Read Trial 2\n",sumTime);
     fclose(fp);
     fp = fopen("benchmark1000-2.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 1000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -90,14 +99,16 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/1000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 1000;
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n1000 Read Trial 3\n",sumTime);
     fp = fopen("benchmark1000-3.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 1000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -106,8 +117,9 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/1000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 1000;
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n10000 Read Trial 1\n",sumTime);
@@ -115,6 +127,7 @@ int main(void){
     fp = fopen("benchmark10000-1.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 10000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -123,14 +136,16 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/10000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/10000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 10000;
     free(mb);
     printf("\nMicrosecond Average Final = %lu\n10000 Read Trial 2\n",sumTime);
     fclose(fp);
     fp = fopen("benchmark10000-2.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 10000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -139,14 +154,16 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/10000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/10000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 10000;
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n10000 Read Trial 3\n",sumTime);
     fp = fopen("benchmark10000-3.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 10000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -155,8 +172,9 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/10000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/10000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
     }
+    sumTime = subTotal / 10000;
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n100000 Read Trial 1\n",sumTime);
@@ -164,6 +182,7 @@ int main(void){
     fp = fopen("benchmark100000-1.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 100000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -172,14 +191,20 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/100000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/100000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+        if((i%10000)==0){
+            sumTime = sumTime + (subTotal/100000);
+            subTotal = 0;
+        }
     }
+    sumTime = sumTime + (subTotal/100000);
     free(mb);
     printf("\nMicrosecond Average Final = %lu\n100000 Read Trial 2\n",sumTime);
     fclose(fp);
     fp = fopen("benchmark100000-2.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 100000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -188,14 +213,20 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/100000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/100000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+        if((i%10000)==0){
+            sumTime = sumTime + (subTotal/100000);
+            subTotal = 0;
+        }
     }
+    sumTime = sumTime + (subTotal/100000);
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n100000 Read Trial 3\n",sumTime);
     fp = fopen("benchmark100000-3.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 100000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -204,8 +235,13 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/100000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/100000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+        if((i%10000)==0){
+            sumTime = sumTime + (subTotal/100000);
+            subTotal = 0;
+        }
     }
+    sumTime = sumTime + (subTotal/100000);
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n1000000 Read Trial 1\n",sumTime);
@@ -213,6 +249,7 @@ int main(void){
     fp = fopen("benchmark1000000-1.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 1000000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -221,14 +258,20 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/1000000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+        if((i%10000)==0){
+            sumTime = sumTime + (subTotal/1000000);
+            subTotal = 0;
+        }
     }
+    sumTime = sumTime + (subTotal/1000000);
     free(mb);
     printf("\nMicrosecond Average Final = %lu\n1000000 Read Trial 2\n",sumTime);
     fclose(fp);
     fp = fopen("benchmark1000000-2.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 1000000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -237,14 +280,20 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/1000000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+        if((i%10000)==0){
+            sumTime = sumTime + (subTotal/1000000);
+            subTotal = 0;
+        }
     }
+    sumTime = sumTime + (subTotal/1000000);
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n1000000 Read Trial 3\n",sumTime);
     fp = fopen("benchmark1000000-3.btree","r+");
     btree_readDB(fp, &mb, &size_mb);
     sumTime = 0;
+    subTotal = 0;
     for(long long i = 0; i < 1000000; i++){
         for(int j = 0; j < 64; j++){
             addKey[j] = rand();
@@ -253,8 +302,13 @@ int main(void){
         gettimeofday(&start, NULL);
         printf("%lli/1000000: Result of attempting to find random key: %u\r", i, btree_findkey(fp, addKey, addRet));
         gettimeofday(&stop, NULL);
-        sumTime = sumTime + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000000;
+        subTotal = subTotal + ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+        if((i%10000)==0){
+            sumTime = sumTime + (subTotal/1000000);
+            subTotal = 0;
+        }
     }
+    sumTime = sumTime + (subTotal/1000000);
     free(mb);
     fclose(fp);
     printf("\nMicrosecond Average Final = %lu\n",sumTime);
